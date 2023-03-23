a44d2948e7c964e2610b4e305a2205853d2bcb8f
Ameetlohana <127467796+Ameetlohana@users.noreply.github.com>
2023-03-23 15:25:45 +1100

    server.js

--- server.js
+++ server.js
@@ -1,23 +1,23 @@
 var express = require('express');
 var app = express();
-var port = process.env.port || 3000;
+var port = process.env.port || 4050;
 
-app.use(express.static("public_html"));
+app.use(express.static(__dirname+'/public_html'));
 app.use(express.json());
-app.use(express.urlencoded({extended:false}));
+app.use(express.urlencoded({extends:false}));
 
 app.get('/', function(req,res) {
     res.render('index.html');
 });
 
-function complexMath(num1, num2) {
-    return parseInt(num1) = parseInt(num2);
+function addNumbers(number1, number2) {
+    return number1 + number2;
     }
 
     app.get('/addTwoNumbers', (req, res) => {
         var number1 = req.query.number1;
         var number2 = req.query.number2;
-        var result = complexMath(number1,number2);
+        var result = addNumbers(number1,number2);
         res.json({statusCode:200, data:result, message: 'success'});
     });
 
